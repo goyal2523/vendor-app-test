46b5142bebb125595a4c6273a6421b4205eae985
Tarun <tarungoyal.wipro@gamil.com>
2017-04-12 09:23:38 +0530

    Added getters and setters for the properties

--- src/Employee.java
+++ src/Employee.java
@@ -16,6 +16,26 @@ public class Employee {
 		this.empId = empId;
 		this.empName = empName;
 	}
+
+
+	public int getEmpId() {
+		return empId;
+	}
+
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+
+	public String getEmpName() {
+		return empName;
+	}
+
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
 	
 	
 	
