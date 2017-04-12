638487b1c9c9fcc9bf08439fd74d462bfe7de292
Tarun <tarungoyal.wipro@gamil.com>
2017-04-12 09:20:26 +0530

    Added default constructor

--- src/Employee.java
+++ src/Employee.java
@@ -4,6 +4,13 @@ public class Employee {
 	
 	private int empId;
 	private String empName;
+	
+
+	public Employee() {
+		super();
+	}
+
+
 	public Employee(int empId, String empName) {
 		super();
 		this.empId = empId;
