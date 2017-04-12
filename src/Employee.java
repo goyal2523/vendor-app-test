05ed8036312475735c3b490ab71695e3e5bda959
Tarun <tarungoyal.wipro@gamil.com>
2017-04-12 09:19:39 +0530

    First Change

---
+++ src/Employee.java
@@ -0,0 +1,16 @@
+
+public class Employee {
+
+	
+	private int empId;
+	private String empName;
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+	
+	
+	
+	
+}
